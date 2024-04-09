e6c6b30b71c6f3b4e96adaaf52e9ceb24f172e19
shhhhreya <shreya.singh.cseaiml.2020@miet.ac.in>
2024-04-09 20:03:30 +0530

    color clicker

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,26 @@ import ShoppingList from './ShoppingList'
 import PropertyList from './PropertyList';
 import Clicker from './Clicker';
 import Toggler from './Toggler';
-
+import ColorBoxes from './ColorBoxes';
+import ColorBox from './ColorBox';
+const colors=[
+  "#fff100",
+  "#ff8c00",
+  "	#e81123",
+  "magenta",
+  "#68217a",
+  "00b294",
+  "cyan",
+  "lime",
+  "teal",
+  "green"
+];
 function App() {
 
   return (
     <div>
-      <Toggler/>
+      <h2>Click and match all the boxes!</h2>
+      <ColorBoxes colors={colors} />
     </div>
 )
 }
---
+++ src/ColorBoxes.jsx
@@ -0,0 +1,13 @@
+import ColorBox from "./ColorBox"
+import './ColorBoxes.css'
+
+export default function ColorBoxes({colors}){
+    const boxes = [];
+    for(let i=0; i<25; i++){
+        boxes.push(<ColorBox colors={colors}/>)
+    }
+    console.log(boxes);
+    return <div className="ColorBoxes">
+        {boxes}
+    </div>;
+}
